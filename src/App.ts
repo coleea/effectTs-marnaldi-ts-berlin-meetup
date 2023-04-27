15a9b4f544f579b6e4a335ebfc9197d10b01e4b8
GUKBEOM LEE <gukbeomlee@GUKBEOMui-Macmini.local>
2023-04-28 07:20:23 +0900

    1st commit

---
+++ src/App.ts
@@ -0,0 +1,29 @@
+import * as Either from '@effect/data/Either'
+// import * as IO from '@effect/io/IO'
+
+type IO<A> = () => A
+
+interface ConsoleService {
+  Console : {
+    log : (msg : string) => IO<Either.Either<never, void>>
+  }
+}
+
+export function helloworld(name:string) {
+  return ({ Console } : ConsoleService ) => Console.log(`hello world : ${name}`)
+  // return () : Either.Either<never, void> => {
+  //   console.log(`Hello ${name}`);  
+  //   return Either.right(undefined)
+  // }
+}
+
+const call = helloworld('lkb');
+
+const r = call({ Console : {
+  log : (name : string) => () => { 
+    console.log(name)
+    return Either.right(undefined)
+  }
+} })
+
+const result = r();
